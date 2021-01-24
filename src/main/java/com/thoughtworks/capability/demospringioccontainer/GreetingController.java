b0342ce5624995fc92f75ece1e4d21f4f9d190b8
Crazy Ryan <>
2021-01-24 19:22:13 +0800

    solution 1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,36 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SINGLETON;
+
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+@Scope(SCOPE_SINGLETON)
+public class GreetingController implements ApplicationContextAware {
 
+    private ApplicationContext applicationContext;
     private final GreetingService greetingService;
 
     @Autowired
     public GreetingController(GreetingService greetingService) {
+        System.out.println("Creating an instance of Greeting Controller");
         this.greetingService = greetingService;
+    }
+
+    @Override
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        return applicationContext.getBean(GreetingService.class).sayHi();
     }
 
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,17 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(SCOPE_PROTOTYPE)
 public class GreetingService {
+
+    public GreetingService() {
+        System.out.println("Creating an instance of GreetingService");
+    }
 
     String sayHi() {
         return "hello world";
